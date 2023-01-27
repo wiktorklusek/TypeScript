bf0c023c06f82bbca355bceef8d9c054d86b10e3
wiktorklusek <63818570+wiktorklusek@users.noreply.github.com>
2023-01-27 19:31:30 +0000

    Create Challenge 1: Classes.ts

---
+++ Challenge 1: Classes.ts
@@ -0,0 +1,23 @@
+/**
+ * -----------------------------
+ * Challenge 1: Classes
+ * -----------------------------
+ * 1. Create a User class.
+ * 2. Add a firstName, lastName, and email property.
+ * 3. Add a get to return fullName.
+ * 4. Add a method to check if your email param matches the user's current email.
+ */
+
+export class User {
+    firstName: string;
+    lastName: string;
+    email: string;
+
+    get fullName(): string {
+        return '${this.firstName} ${this.lastName}'
+    }
+
+    doesEmailMatch(email: string): boolean {
+        return this.email === email;
+    }
+}
